d31c4c06cc23a920a412cde8f8f98a005ab0acaa
Rishat <rishatl@inbox.ru>
2021-11-20 00:43:24 +0300

    Initialized project

---
+++ RetainCycleExample/RetainCycleExample/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RetainCycleExample
+//
+//  Created by Rishat on 16.11.2021.
+//
+
+import UIKit
+
+//MARK: - SomeViewController
+
+final class ViewController: UIViewController {
+
+    //MARK: - Initialization
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+
+    }
+    
+}
